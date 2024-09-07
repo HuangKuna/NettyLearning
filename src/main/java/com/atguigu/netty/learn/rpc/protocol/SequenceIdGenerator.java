cd4609366176c346eef56dd1e99270eff333ee32
HuangKun <1095143643@qq.com>
2024-09-07 20:14:03 +0800

    九月七号学习:聊天部分

---
+++ src/main/java/com/atguigu/netty/learn/rpc/protocol/SequenceIdGenerator.java
@@ -0,0 +1,11 @@
+package com.atguigu.netty.learn.rpc.protocol;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public abstract class SequenceIdGenerator {
+    private static final AtomicInteger id = new AtomicInteger();
+
+    public static int nextId() {
+        return id.incrementAndGet();
+    }
+}
